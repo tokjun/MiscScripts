37d4d1863f913fb3a69f6913ab5caad24478794c
Junichi Tokuda <tokuda@bwh.harvard.edu>
2016-09-28 14:58:44 -0400

    Remove pre-fixed parameters.

--- dicom_split.sh
+++ dicom_split.sh
@@ -1,19 +1,38 @@
 #! /bin/bash
+#
+# Use this script to separate phase image from magnitude images. The script 
+#
+# Usage:  dicom_split.sh <ORG_DIR> <DST_DIR>
+#
+
+if [ "$#" -ne 2 ] || ! [ -d "$1" ]; then
+    echo "Usage: $0 <ORG_DIR> <DST_DIR>" >&2
+    exit 1
+fi
+
+ORG_DIR=$1
+DST_DIR=$2
 
 
-for i in {12..219}
-do
-    DIR="Kidney2echoPETRA21MM3CRYO11000SPKFOV35GRAD9_"$i
-    if [ -d $DIR ]; then
-	echo "Processing "$DIR"..."
-	mkdir $DIR/MAG
-	mkdir $DIR/PH
-	cd $DIR
-	find -f *.dcm -exec sh -c 'RET=`dcmdump +P "0008,0008" $1`; if echo "$RET" | grep "M\\\NORM" >/dev/null ; then cp $1 MAG; else cp $1 PH; fi' -- {} \;
-	mv MAG ../MAG_$i
-	mv PH ../PH_$i
-	cd ..
-    fi
-done
+if ! [ -d $ORG_DIR ]; then
+    echo $ORG_DIR "does not exist..."
+    exit 1
+fi
+
+if ! [ -d $DST_DIR ]; then
+    mkdir $DST_DIR
+fi
+
+echo "Processing "$ORG_DIR"..."
+if ! [ -d $DST_DIR/MAG ]; then
+    mkdir $DST_DIR/MAG
+fi
+if ! [ -d $DST_DIR/PH ]; then
+    mkdir $DST_DIR/PH
+fi
+cd $DST_DIR
+find ../$ORG_DIR/* -type f -exec sh -c 'RET=`dcmdump +P "0008,0008" $1`; if echo "$RET" | grep "M\\\NORM" >/dev/null ; then cp $1 MAG; else cp $1 PH; fi' -- {} \;
+cd ..
+
     
 
