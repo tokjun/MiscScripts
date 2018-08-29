bac2abbcf859beeae96f92ae67133836b2eff0ca
Junichi Tokuda <tokuda@bwh.harvard.edu>
2018-08-29 10:12:40 -0400

    Renamed dicom_list_value.sh to dicom_list_max_value.sh as it only lists the max values for the second tag. Added a new script to list the values of the specified tags for all files in the folder.

--- dicom_list_max_values.sh
+++ dicom_list_max_values.sh
@@ -24,13 +24,34 @@ echo "Processing $INPUT_DIR ..."
 
 LIST=`find $INPUT_DIR -depth 1 -type f`
 
-for FILE in $LIST
+unset AVALUE2
+unset AFILENAME
+declare -A AVALUE2
+declare -A AFILEPATH
+
+for FILEPATH in $LIST
 do
+    SIUID=_`dcmdump +P '0020,000E' $FILEPATH| sed -e 's/.*\[\(.*\)\].*/\1/'` #Series Instance UID
+    VALUE2=`dcmdump +P $TAG_2 $FILEPATH| sed -e 's/.*\[\(.*\)\].*/\1/'`
+
+    if [[ ${AVALUE2[$SIUID]} == "" ]]; then
+	AVALUE2[$SIUID]="$VALUE2"
+	AFILEPATH[$SIUID]="$FILEPATH"
+    elif [[ ${AVALUE2[$SIUID]} > $VALUE2 ]]; then
+	AVALUE2[$SIUID]=$VALUE2
+	AFILEPATH[$SIUID]="$FILEPATH"
+    fi
+    echo ${AFILEPATH[$SIUID]}, $VALUE2
+done
+
+for key in "${!AVALUE2[@]}";
+do
+    SIUID=$key
+    VALUE2=${AVALUE2[$SIUID]}
+    FILE=${AFILEPATH[$SIUID]}
     STUDY=`dcmdump +P '0020,0010' $FILE| sed -e 's/.*\[\(.*\)\].*/\1/'`
     SERIES=`dcmdump +P '0020,0011' $FILE| sed -e 's/.*\[\(.*\)\].*/\1/'`
     VALUE1=`dcmdump +P $TAG_1 $FILE| sed -e 's/.*\[\(.*\)\].*/\1/'`
-    VALUE2=`dcmdump +P $TAG_2 $FILE| sed -e 's/.*\[\(.*\)\].*/\1/'`
-
+    
     echo $STUDY,$SERIES,$VALUE1,$VALUE2
 done
-
