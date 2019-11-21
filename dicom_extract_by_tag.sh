9fe4e12e499edef606ce2672d5adecec8d136671
Junichi Tokuda <tokuda@bwh.harvard.edu>
2019-11-20 23:10:35 -0500

    dicom_extract_by_tag.sh Specify command to execute

--- dicom_extract_by_tag.sh
+++ dicom_extract_by_tag.sh
@@ -1,33 +1,42 @@
 #! /bin/bash
 
 #
-# Usage:  dicom_extract_by_tag.sh <INPUT_DIR> <DICOM_TAG> <KEY>
+# Usage:  dicom_extract_by_tag.sh <INPUT_DIR> <DICOM_TAG> <KEY> <COMMAND>
 #
-#   Ex)  dicom_extract_by_tag.sh . "0008,103e" "TEMPLATE"
+#   Ex)  dicom_extract_by_tag.sh . "0008,103e" "TEMPLATE" cp
 #   Do not add '/' at the end of <INPUT_DIR>
 
-if [ "$#" -ne 3 ] || ! [ -d "$1" ]; then
-  echo "Usage: $0 <INPUT_DIR> <DICOM_TAG> <KEY>" >&2
+if [[ "$#" -ne 4 ]] || ! [[ -d "$1" ]]; then
+  echo "Usage: $0 <INPUT_DIR> <DICOM_TAG> <KEY> <COMMAND>" >&2
   exit 1
 fi
 
 INPUT_DIR=$1
 export DEBDTAG=$2
 export DEBDKEY=$3
+export COMMAND=$4
+
+export GREPSTR="$DEBDKEY"
 
 LIST=`find $INPUT_DIR -depth 1 -type d`
 
-if [$LIST == ""]; then
+if [[ $LIST == "" ]]; then
     LIST=$INPUT_DIR
 fi
+
+# Extended regular expression -
+# -r GNU
+# -E FreeBSD, NetBSD, OpenBSD, and OS X
+SUFFIX=`echo $DEBDKEY |sed -E 's/\;|\%/-/g'`
+
 
 for dir in $LIST
 do
     echo "Processing $dir ..."
-    export DEBDNEWDIR=$dir"-"$DEBDKEY
+    export DEBDNEWDIR=$dir"-"$SUFFIX
     mkdir $DEBDNEWDIR
     
-    find $dir/* -type f -exec sh -c 'RET=`dcmdump -q +P "$DEBDTAG" "$1"`; if echo "$RET" | grep "$DEBDKEY" >/dev/null ; then cp "$1" "$DEBDNEWDIR"; fi' -- {} \;
+    find $dir/* -type f -exec sh -c 'RET=`dcmdump -q +P "$DEBDTAG" "$1"`; if echo "$RET" | grep "$GREPSTR" >/dev/null ; then $COMMAND "$1" "$DEBDNEWDIR"; fi' -- {} \;
     
 done
     
